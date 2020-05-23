06b243d761ecb981f1a9e585c5bf130bd43b8488
hitma-n <yomanspirit@gmail.com>
2020-05-23 19:50:40 +0530

    Use email:abc@gmail.com pass:1234567890 For testing purposes

--- app/src/main/java/com/example/clgapp/LogedIN.java
+++ app/src/main/java/com/example/clgapp/LogedIN.java
@@ -1,16 +1,12 @@
 package com.example.clgapp;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-
-
 import android.view.Menu;
 import android.view.MenuItem;
+
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
-
-import java.util.Objects;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -32,16 +28,16 @@ public class LogedIN extends AppCompatActivity
         //Objects.requireNonNull(this.getSupportActionBar()).setTitle("Marks");
 
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
         //By default marks is selected
@@ -54,7 +50,7 @@ public class LogedIN extends AppCompatActivity
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -106,10 +102,13 @@ public class LogedIN extends AppCompatActivity
             startActivity(new Intent(LogedIN.this,MainActivity.class));
             finish();
             return true;
+        } else if (id == R.id.calender) {
+            //Calender Fragment
+            selectedFrag = new Calender();
         }
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_display,selectedFrag).commit();
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
 
         drawer.closeDrawer(GravityCompat.START);
         return true;
